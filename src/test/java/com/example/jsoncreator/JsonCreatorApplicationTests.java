a4bbbd102ad232999e1ffe75fa6953dd99fb0924
mohamadp91 <69380537+mohamadp91@users.noreply.github.com>
2022-08-12 10:56:17 +0430

    initialize the project (#1)

---
+++ src/test/java/com/example/jsoncreator/JsonCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsoncreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonCreatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
