a5e2902c743484b45e7f9343536f927672ffb536
mohamadp91 <mohamad7291m@gmail.com>
2022-08-12 10:53:55 +0430

    initialize the project

---
+++ src/main/java/com/example/jsoncreator/JsonCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsoncreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonCreatorApplication.class, args);
+    }
+
+}
